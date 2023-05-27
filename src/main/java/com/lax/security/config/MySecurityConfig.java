d9b61f3e9df4fc962165e2470b285219271c9bd5
lax <codewithlax@gmail.com>
2023-05-27 18:43:11 +0530

    Basic Auth & Form based Auth & Form based login custom pages

--- src/main/java/com/lax/security/config/MySecurityConfig.java
+++ src/main/java/com/lax/security/config/MySecurityConfig.java
@@ -1,18 +1,15 @@
 package com.lax.security.config;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,16 +22,34 @@ public class MySecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                //.csrf().disable()
+
+                //Get the cookies XSRF-TOKEN value
+                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .and()
+
                 .authorizeRequests()
                 // for "/public/**" url it will not ask for username and password
                 //.antMatchers("/public/**").permitAll()
 
                 //Only user how have admin role can access the "/public/**" URL
-                .antMatchers("/public/**").hasRole("ADMIN")
+                .antMatchers("/sign-in/**").permitAll()
+                .antMatchers("/public/**").hasRole("USER")
+                .antMatchers("/users/**").hasRole("ADMIN")
                 .anyRequest()
                 .authenticated()
                 .and()
-                .httpBasic();
+                //Basic Auth
+                //.httpBasic();
+
+                //Form based Auth
+                .formLogin()
+
+                //Form based login custom pages
+                .loginPage("/sign-in")
+                .loginProcessingUrl("/do-login")
+                .defaultSuccessUrl("/users/");
+
         return http.build();
     }
 
